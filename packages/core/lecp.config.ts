a487d9e2d5b9ff18ea5fbb6785003f1003fa3e42
dr2009 <dr2009.china@gmail.com>
2024-12-19 15:56:14 +0800

    chore: remove useless config

--- packages/core/lecp.config.ts
+++ packages/core/lecp.config.ts
@@ -1,37 +1,9 @@
 import { defineConfig } from "./src/define-config.ts";
 
-import pkg from "./package.json" assert { type: "json" };
-
 export default defineConfig({
-	format: [
-		{
-			type: "esm",
-			// builder: "swc",
-			// outDir: "dist/es",
-		},
-		{
-			type: "cjs",
-			// builder: "swc",
-			// outDir: "dist/lib",
-			// minify: true,
-		},
-	],
-	define: {
-		"typeof window": "object",
-		__DEV__: JSON.stringify(true),
-		"process.env.NODE_ENV": JSON.stringify("development"),
-	},
-	css: {
-		// [name]__[local]___[hash:base64:5]
-		cssModules: `${pkg.name.replace("@", "").replace("/", "__")}__[local]`,
-		lessCompile: true,
-	},
-	dts: {
-		mode: "bundless",
-		builder: "ts",
-	},
+	format: [{ type: "esm" }, { type: "cjs" }],
+	dts: true,
 	targets: {
 		node: "20.11.0",
-		// chrome: 55,
 	},
 }) as unknown;
