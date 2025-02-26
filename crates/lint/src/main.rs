47e47e1499a907395cc0f0037f9b9891bac521b2
ityuany <519495771@qq.com>
2025-02-26 21:05:29 +0800

    feat: 添加文件树构建功能
    - 更新了 `Cargo.lock`，增加了多个新的依赖，包括 `ptree` 和相关的显示配置工具。
    - 在 `Cargo.toml` 和 `crates/lint/Cargo.toml` 中添加了 `ptree` 依赖。
    - 在 `crates/lint/src/main.rs` 中新增了构建和打印文件树的功能，使用了 `ptree` crate。
    - 将主函数中的 `lint` 调用替换为 `test_tree` 测试文件树功能。

--- crates/lint/src/main.rs
+++ crates/lint/src/main.rs
@@ -1,6 +1,8 @@
 #![recursion_limit = "512"]
 use std::{
     env::current_dir,
+    fs,
+    ops::Index,
     path::{Path, PathBuf},
     rc::Rc,
     sync::Arc,
@@ -20,10 +22,91 @@ use oxc_linter::LintPlugins;
 use oxc_parser::Parser;
 use oxc_semantic::SemanticBuilder;
 
+use ptree::{
+    print_config::{ASCII_CHARS_TICK, UTF_CHARS, UTF_CHARS_BOLD},
+    print_tree_with, write_tree_with, Color, PrintConfig, Style, TreeBuilder,
+};
 use serde_json::json;
 
-fn main() {
+fn build_tree(path: &Path, tree: &mut TreeBuilder) -> std::io::Result<()> {
+    if path.is_dir() {
+        tree.begin_child(format!("{}/", path.file_name().unwrap().to_string_lossy()));
+
+        let mut entries: Vec<_> = fs::read_dir(path)?.filter_map(|e| e.ok()).collect();
+
+        // 排序确保输出一致
+        entries.sort_by_key(|e| e.path());
+
+        for entry in entries {
+            build_tree(&entry.path(), tree)?;
+        }
+
+        tree.end_child();
+    } else {
+        tree.add_empty_child(path.file_name().unwrap().to_string_lossy().to_string());
+    }
+
+    Ok(())
+}
+
+fn test_tree() -> std::io::Result<()> {
+    let mut tree = TreeBuilder::new(String::from("."));
+    let cwd = current_dir().unwrap().join("examples/demo-component/src");
+    build_tree(&cwd, &mut tree)?;
+
+    let tree = tree.build();
+
+    let config = {
+        let mut config = PrintConfig::from_env();
+        config.branch = Style {
+            // foreground: Some(Color::Red),
+            // background: Some(Color::Yellow),
+            // dimmed: true,
+            bold: false,
+            ..Style::default()
+        };
+        config.leaf = Style {
+            bold: false,
+            ..Style::default()
+        };
+        config.characters = UTF_CHARS.into();
+        config.indent = 4;
+        config
+    };
+
+    // print_tree_with(&tree, &config).expect("Error printing tree");
+
+    let mut res = Vec::new();
+    write_tree_with(&tree, &mut res, &config).expect("Error writing tree");
+    let tree_str = String::from_utf8(res).unwrap();
+    // println!("{} {}", tree_str, tree_str.find("Demo.less").unwrap());
+
+    let file_name = "Demo.less";
+    let index = tree_str.find(file_name).unwrap();
+
+    let report = miette!(
+        severity = miette::Severity::Error,
+        code = "lego/duplicate-file-names",
+        help = "Check for duplicate file names",
+        labels = vec![
+            LabeledSpan::at(
+                index..index + file_name.len(),
+                "Duplicate file names, different suffixes"
+            ),
+            // LabeledSpan::at(122..130, "Duplicate file names, different suffixes"),
+        ],
+        "Duplicate file names",
+    )
+    .with_source_code(tree_str);
+
+    eprintln!("{:?}", report);
+
+    Ok(())
+}
+
+fn lint() {
     // 统计执行耗时
+
     let start = Instant::now();
 
     let cwd = current_dir().unwrap();
@@ -53,6 +136,12 @@ fn main() {
     let end = Instant::now();
 
     println!("执行耗时: {:?}", end - start);
+}
+
+fn main() {
+    test_tree().unwrap();
+
+    // lint();
 
     //     let report = miette!(
     //         severity = miette::Severity::Error,
